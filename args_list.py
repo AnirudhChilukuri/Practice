8728d0de249815d1f07b7e03998d7d26e805d36e
Anirudh Chilukuri <Anirudh@Anirudhs-MacBook-Air.local>
2021-10-21 17:04:06 +0530

    Classification

--- args_list.py
+++ args_list.py
@@ -4,4 +4,4 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--para1", type=int, nargs='+')
 args = vars(parser.parse_args())
 
-print(sum(args['para1']))
+print(sum(args['para1'])) 
---
+++ cls1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+"Anirudh"
+smell = 10
+color = 5
+shape = 7
+taste = 10
+size = 9
+"Sample"
+smell = 7
+color = 7
+shape = 7
+taste = 7
+size = 7
+"Akhilesh"
+smell = 6
+color = 2
+shape = 10
+taste = 10
+size = 8
